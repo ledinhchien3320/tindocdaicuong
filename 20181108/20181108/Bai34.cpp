78b016c18064f616abf026e5981297e337c5f6be
LeDinhChien <thelastone3320@gmail.com>
2018-11-08 17:38:26 +0700

    no message

---
+++ 20181108/20181108/Bai34.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//Bai34 tinh tong s=canbac2(n+ canbac2((n-1)+...+canbac2(1)) co n dau can
+	//khai bao tong S va nhap n
+	int n;
+	float S = 0;
+	//chay vong for
+	for (int i = 1; i <= n; i++) {
+		S = sqrt(i + S);
+	}
+	cout << "Cau34= " << S << endl;
+	system("pause");
+	return true;
+}
