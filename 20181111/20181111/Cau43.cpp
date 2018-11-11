30a275f6a8f91346c2370fe3ee99943dc1fac017
LeDinhChien <thelastone3320@gmail.com>
2018-11-11 18:14:16 +0700

    no message

--- 20181111/20181111/Cau43.cpp
+++ 20181111/20181111/Cau43.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 	//dem so chu so cua so nguyen duong n
 	int demso(int n) {
+		//khai bao bien dem
 		int dem = 0;
+		//chay vong lap while de giam gia tri cua n di 10 la
 		while (n) {
 			int i = n / 10;
 			dem++;
@@ -11,9 +13,11 @@ using namespace std;
 		return dem;
 	}
 	int main() {
+		//khai bao va nhap n
 		int n;
 	cout << "nhap mot chu so: ";
 	cin >> n;
+	//gan biet Result cho KQ cua ct o tren va in ra ket qua
 	int Result = demso(n);
 	cout << "so vua nhap co " << Result << " chu so "<<endl;
 	system("pause");
