a4545f689630c1f82412a07bf23dcc8f8e1bd657
ledinhchien3320 <44728489+ledinhchien3320@users.noreply.github.com>
2018-11-28 18:46:51 +0700

    Add files via upload

---
+++ 20181128/Cau95.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	//viet ct nhap vao 3 so thuc hay thay tat ca cac so am bang gia tri tuyet doi cua no
+	int a, b, c;
+	cout << "Moi nhap 3 so:";
+	cin >> a >> b >> c;
+	cout << "a= " << abs(a) << endl;
+	cout << "b= " << abs(b) << endl;
+	cout << "c= " << abs(c) << endl;
+	system("pause");
+	return true;
+}
